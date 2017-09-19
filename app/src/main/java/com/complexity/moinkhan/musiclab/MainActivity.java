463483d288c20beae70f1688e2c14741d7d553c7
Moin Khan <moin9997594451@gmail.com>
2017-09-19 21:55:07 +0530

    Final Music player built that loads songs from your sdcard

--- app/src/main/java/com/complexity/moinkhan/musiclab/MainActivity.java
+++ app/src/main/java/com/complexity/moinkhan/musiclab/MainActivity.java
@@ -1,13 +1,157 @@
 package com.complexity.moinkhan.musiclab;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Songinfo> songs = new ArrayList<Songinfo>();
+    RecyclerView recyclerView;
+    SeekBar seekBar;
+    SongAdapter songAdapter;
+    MediaPlayer mediaPlayer;
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+
+        songAdapter = new SongAdapter(this,songs);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),linearLayoutManager.getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(songAdapter);
+
+        songAdapter.setOnitemClicklistener(new SongAdapter.OnitemClickListener() {
+            @Override
+            public void onItemclick(final Button b, View v, final Songinfo obj, int position) {
+                Runnable r = new Runnable() {
+                    @Override
+                    public void run() {
+                        mediaPlayer = new MediaPlayer();
+                        try {
+                            if (b.getText().toString().equals("Stop")) {
+                                b.setText("Play");
+                                mediaPlayer.stop();
+                                mediaPlayer.reset();
+                                mediaPlayer.release();
+                                mediaPlayer = null;
+                            } else {
+                                mediaPlayer.setDataSource(obj.getSongUrl());
+                                mediaPlayer.prepareAsync();
+                                mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                                    @Override
+                                    public void onPrepared(MediaPlayer mp) {
+                                        mp.start();
+                                        seekBar.setProgress(0);
+                                        seekBar.setMax(mp.getDuration());
+                                    }
+                                });
+                                b.setText("Stop");
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                handler.postDelayed(r,100);
+
+            }
+        });
+
+        CheckPermission();
+
+        Thread t = new MyThread();
+        t.start();
+    }
+
+    private class MyThread extends Thread{
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(1000);
+                if (mediaPlayer != null) {
+                    seekBar.setProgress(mediaPlayer.getCurrentPosition());
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private void CheckPermission() {
+        if (Build.VERSION.SDK_INT >= 23){
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE ) != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},123);
+                return;
+            }
+        }else{
+            loadSongs();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 123:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    loadSongs();
+                }else{
+                    Toast.makeText(this, "permission denied", Toast.LENGTH_SHORT).show();
+                    CheckPermission();
+                }
+                break;
+
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+    }
+
+    private void loadSongs() {
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String selection = MediaStore.Audio.Media.IS_MUSIC+"!=0";
+        Cursor cursor = getContentResolver().query(uri,null,selection,null,null);
+
+        if (cursor != null){
+            if (cursor.moveToFirst()){
+                do {
+                    String name = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME));
+                    String artist = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST));
+                    String url = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA));
+
+                    Songinfo s = new Songinfo(name,artist,url);
+                    songs.add(s);
+                }while (cursor.moveToNext());
+            }
+            cursor.close();
+            songAdapter = new SongAdapter(this,songs);
+        }
     }
 }
